aeec78eb707ea731e0a0022f2e6d29f4d128a13e
vSKAH <vskahhh@gmail.com>
2024-04-09 23:29:40 +0200

    docs(HPP): Adding documentation

--- Includes/Builders/UserBuilder.hpp
+++ Includes/Builders/UserBuilder.hpp
@@ -1,10 +1,19 @@
 #ifndef USERBUILDER_HPP
 	#define USERBUILDER_HPP
-
 	#include <string>
 	#include "User.hpp"
 	#include "UserProperties.hpp"
-	class UserBuilder {
+/**
+ * @class UserBuilder
+ * @brief A builder class for creating User objects.
+ * @author jbadaire
+ *
+ * This class provides a way to construct User objects in a step-by-step manner.
+ * It includes setter methods for each property of the User class, and a build method
+ * to finally create the User object.
+ */
+	class UserBuilder
+	{
 		private:
 			std::string name;
 			std::string realName;
@@ -16,13 +25,67 @@
 
 		public:
 			UserBuilder();
-			UserBuilder& setName(const std::string& name);
-			UserBuilder& setRealName(const std::string& realName);
-			UserBuilder& setNickname(const std::string& nickname);
-			UserBuilder& setIpAddr(const std::string& ipAddr);
-			UserBuilder& setUserSocketId(int userSocketFd);
-			UserBuilder& setProperties(const UserProperties& properties);
+
+			/**
+			 * @brief Sets the name of the User.
+			 * @param name The name to set.
+			 * @return A reference to the UserBuilder object.
+			 * @author jbadaire
+			 */
+			UserBuilder &setName(const std::string &name);
+
+			/**
+			 * @brief Sets the real name of the User.
+			 * @param realName The real name to set.
+			 * @return A reference to the UserBuilder object.
+			 * @author jbadaire
+			 */
+			UserBuilder &setRealName(const std::string &realName);
+
+			/**
+			 * @brief Sets the nickname of the User.
+			 * @param nickname The nickname to set.
+			 * @return A reference to the UserBuilder object.
+			 * @author jbadaire
+			 */
+			UserBuilder &setNickname(const std::string &nickname);
+
+			/**
+			 * @brief Sets the IP address of the User.
+			 * @param ipAddr The IP address to set.
+			 * @return A reference to the UserBuilder object.
+			 * @author jbadaire
+			 */
+			UserBuilder &setIpAddr(const std::string &ipAddr);
+
+			/**
+			 * @brief Sets the socket ID of the User.
+			 * @param userSocketFd The socket ID to set.
+			 * @return A reference to the UserBuilder object.
+			 * @author jbadaire
+			 */
+			UserBuilder &setUserSocketId(int userSocketFd);
+
+			/**
+			 * @brief Sets the properties of the User.
+			 * @param properties The properties to set.
+			 * @return A reference to the UserBuilder object.
+			 * @author jbadaire
+			 */
+			UserBuilder &setProperties(const UserProperties &properties);
+
+			/**
+			 * @brief Clears the builder for reuse.
+			 * @author jbadaire
+			 */
 			void clearBuilder();
-			User& build();
+
+			/**
+			 * @brief Builds the User object.
+			 * @return A reference to the built User object.
+			 * @throws UserBuildException if essentials properties for IRC are invalid.
+			 * @author jbadaire
+			 */
+			User &build();
 	};
 #endif
