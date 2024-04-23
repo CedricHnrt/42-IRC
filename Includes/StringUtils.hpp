0e54271629a99f8f321a93b5b81e7769438fb786
vSKAH <vskahhh@gmail.com>
2024-04-23 13:37:32 +0200

    feat(censoring): Add censoring string system

--- Includes/StringUtils.hpp
+++ Includes/StringUtils.hpp
@@ -18,6 +18,8 @@ public:
 	static void replaceAll(std::string &line, const std::string &key, const std::string &value);
 	static std::string ltos(long value);
 	static std::vector<std::string>split(const std::string &input, int c);
+	static std::vector<std::string> generateCensuredStrings(std::vector<std::string> words);
+	static std::pair<bool, std::string> hasCensuredWord(std::string word, std::vector<std::string> censuredWords);
 };
 
 #endif //STRINGUTILS_H
